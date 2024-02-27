9abd6d96041d73ae2b9f3cab605a23114f406053
florien2208 <mahorompakanyiflorien@gmail.com>
2024-02-27 10:13:59 +0200

    first learnig code of type script

---
+++ box.ts
@@ -0,0 +1,6 @@
+var character = "mario";
+console.log(character);
+const inputs = document.querySelectorAll("input");
+inputs.forEach(function (input) {
+  console.log(input);
+});
---
+++ index.ts
@@ -0,0 +1,6 @@
+// console.log("this is dj flo")
+// let age: number=20;
+const character = 'mario';
+console.log(char);
+const inputs = document.querySelectorAll('input');
+console.log(inputs)
